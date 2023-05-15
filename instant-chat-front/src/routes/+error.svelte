e8101e672cfb2c921345c199c86ba1e1c9cdab88
AlexandreHiroyuki <alex.hiroyuki@outlook.com>
2023-05-15 14:29:39 -0300

    feature: error page style

--- instant-chat-front/src/routes/+error.svelte
+++ instant-chat-front/src/routes/+error.svelte
@@ -4,4 +4,9 @@
 	import { page } from '$app/stores';
 </script>
 
-<h1>{$page.status}: {$page.error.message}</h1>
+<h1 class="text-center">{$page.status}: {$page.error.message}</h1>
+
+<p class="text-center mt-2">
+	Sorry for the incovenience! Please, report this error on a issue on the
+	<a href="https://github.com/AlexandreHiroyuki/instant-chat/issues">GitHub repository</a>!
+</p>
